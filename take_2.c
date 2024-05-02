ab5cb1d0a4450bc62a478fe0b82a14ed9c325757
BakijazzmanSE <bakijazman@gmail.com>
2024-05-02 09:17:05 +0100

    Secomd take

---
+++ take_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int add();
+int main()
+{
+    printf("Hello World,My name is Temidayo and this is Introduction to Low level Programming\n");
+        printf("Alx ;low level Programming\n");
+        add();
+        return (0);
+}
+
+int add()
+{
+    int w = 68;
+    int t = 70;
+    int result = w + t;
+   printf("%d\n",result); 
+}
+
+
+
