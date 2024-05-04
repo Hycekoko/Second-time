dd6fc13b64688ccfa2de345956439db32b454f4b
BakijazzmanSE <bakijazman@gmail.com>
2024-05-04 22:04:03 +0100

    first one

---
+++ 1.one.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Firts timr function,This is introduction\n");
+    printf("My name is Temidayo,Mostly calle Hyce and this is my intro to Tech as a whole\n");
+
+    return (0);
+}
+
